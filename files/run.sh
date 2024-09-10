5c8bc1eebcda98dafbe7a4ea824ef769f7e980e2
dbs <github@b4sh.de>
2024-09-10 22:50:35 +0200

    add: git config

--- files/run.sh
+++ files/run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
-echo "starting git daemon in tmux session"
+echo "configure git safe directory and starting git daemon in tmux session"
+git config --system --add safe.directory '*' 
 tmux new-session -d -s git 'git daemon --verbose --base-path=/data --enable=receive-pack'
 echo "starting lighttpd"
 tmux new-session -d -s http '/usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf'
